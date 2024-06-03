ddfe78b634504a216aabb37f79edb31bfbb998ee
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-03 16:40:57 +0100

    chore: changed app theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-  return <p>Hello World</p>;
+  return (
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl"></div>
+    </MaxWidthWrapper>
+  );
 }
