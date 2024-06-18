459a3d7e507626806650b0cac0c150c9c26563c7
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-18 09:07:11 +0100

    feat: signin flow

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
+import { inferAsyncReturnType } from "@trpc/server";
+import { CreateContextOptions } from "vm";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -11,6 +13,8 @@ const createContext = ({
   req,
   res,
 }: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
 const start = async () => {
   const payload = await getPayloadClient({
