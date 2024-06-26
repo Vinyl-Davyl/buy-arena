725690c52ec317bf14fd6eb72161af057160fef6
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-26 12:54:46 +0100

    update: development

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,4 +1,21 @@
-import { CollectionConfig } from "payload/types";
+import { User } from "@/payload-types";
+import { Access, CollectionConfig } from "payload/types";
+
+const isAdminOrHasAccessToImages =
+  (): Access =>
+  async ({ req }) => {
+    const user = req.user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === "admin") return true;
+
+    return {
+      // if image of the user fill equals the image of the currently logged in user, then escentially it's your image
+      user: {
+        equals: req.user.id,
+      },
+    };
+  };
 
 export const Media: CollectionConfig = {
   slug: "media",
@@ -8,6 +25,19 @@ export const Media: CollectionConfig = {
         return { ...data, user: req.user.id };
       },
     ],
+  },
+  access: {
+    read: async ({ req }) => {
+      const referer = req.headers.referer;
+
+      if (!req.user || !referer?.includes("sell")) {
+        return true;
+      }
+
+      return await isAdminOrHasAccessToImages()({ req });
+    },
+    delete: isAdminOrHasAccessToImages(),
+    update: isAdminOrHasAccessToImages(),
   },
   admin: {
     hidden: ({ user }) => user.role !== "admin",
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,10 +3,10 @@ import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
-import { Users } from "./app/collections/Users";
+import { Users } from "./collections/Users";
 import dotenv from "dotenv";
-import { Products } from "./app/collections/Products/Products";
-import { Media } from "./app/collections/Media";
+import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
