2e882c741a97595861582f6fe09cdbea8ea0ded8
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-26 12:16:14 +0100

    update: development

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,8 @@ import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 import { Users } from "./app/collections/Users";
 import dotenv from "dotenv";
+import { Products } from "./app/collections/Products/Products";
+import { Media } from "./app/collections/Media";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -12,7 +14,8 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  // list of all the collection schema calls for CMS(payload setup)
+  collections: [Users, Products, Media],
   routes: {
     admin: "/sell",
   },
