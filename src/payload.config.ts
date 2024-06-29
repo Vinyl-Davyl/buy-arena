3a3a9d662596fda3e249ff2f13edb169d0896b52
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-29 08:31:29 +0100

    update: development

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,7 @@ import { Users } from "./collections/Users";
 import dotenv from "dotenv";
 import { Products } from "./collections/Products/Products";
 import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFile";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -15,7 +16,7 @@ dotenv.config({
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
   // list of all the collection schema calls for CMS(payload setup)
-  collections: [Users, Products, Media],
+  collections: [Users, Products, Media, ProductFiles],
   routes: {
     admin: "/sell",
   },
