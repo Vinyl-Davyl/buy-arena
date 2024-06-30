c20133a88a1a2bb32afbb5e126001ebacec74d6b
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-06-30 02:42:47 +0100

    update: developemnt

---
+++ src/components/ProductReel.tsx
@@ -0,0 +1,27 @@
+interface ProductReelProps {
+  title: string;
+  subtitle?: string;
+}
+
+const ProductReel = (props: ProductReelProps) => {
+  const { title, subtitle } = props;
+
+  return (
+    <section className="py-12">
+      <div className="md:flex md:items-center md:justify-between mb-4">
+        <div className="max-w-2xl px-4 lg:max-w-4xl lg:px-0">
+          {title ? (
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              {title}
+            </h1>
+          ) : null}
+          {subtitle ? (
+            <h1 className="mt-2 text-sm text-muted-foreground">{subtitle}</h1>
+          ) : null}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductReel;
