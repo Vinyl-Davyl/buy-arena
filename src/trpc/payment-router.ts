45a46bc2de637912a97c6696c9735114700be5df
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-07-27 13:56:20 +0100

    update: development

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
-import { getPayloadClient } from "@/get-payload";
-import { stripe } from "@/lib/stripe";
+import { getPayloadClient } from "../get-payload";
+import { stripe } from "../lib/stripe";
 import type Stripe from "stripe";
 
 export const paymentRouter = router({
