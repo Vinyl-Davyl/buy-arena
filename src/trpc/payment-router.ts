21bac82070cb920a4112312516f26107d1809234
Vinyl-Davyl <okononfuadavid@gmail.com>
2024-07-27 13:45:18 +0100

    feat:  setup TRPC router with a createSession procedure for creating a Stripe checkout session.

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "@/get-payload";
+import { stripe } from "@/lib/stripe";
+import type Stripe from "stripe";
 
 export const paymentRouter = router({
   createSession: privateProcedure
@@ -14,6 +16,7 @@ export const paymentRouter = router({
         throw new TRPCError({ code: "BAD_REQUEST" });
       }
 
+      //  Initializes the payload client for database operations.
       const payload = await getPayloadClient();
 
       const { docs: products } = await payload.find({
@@ -24,5 +27,61 @@ export const paymentRouter = router({
           },
         },
       });
+
+      // filters the products to include only those that have a priceId.
+      const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+
+      // creates a new order in the orders collection with the filtered products and user information.
+      const order = await payload.create({
+        collection: "orders",
+        data: {
+          _isPaid: false,
+          products: filteredProducts,
+          user: user.id,
+        },
+      });
+
+      // Initializes an empty array of line items for the Stripe checkout session
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+      // reason: so to push two things, first off, transaction fee, each product’s price ID and quantity to the line_items array.
+
+      filteredProducts.forEach((product) => {
+        line_items.push({
+          // priceId from collection/products
+          price: product.priceId!,
+          quantity: 1,
+        });
+      });
+
+      line_items.push({
+        price: "price_1Ph8PCEt26n6iK6GS5Ia2USe",
+        // paystack
+        // price: "transaction-fee-xvcvst",
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false,
+        },
+      });
+
+      try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ["card", "paypal"],
+          mode: "payment",
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+          },
+          line_items,
+        });
+
+        return { url: stripeSession.url };
+      } catch (err) {
+        console.log(err);
+
+        return { url: null };
+      }
     }),
 });
