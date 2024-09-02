e802964b57cb7b933045ffda4cb06d4ce8ce8eb1
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2024-09-03 00:00:04 +0100

    update: development

--- next.config.js
+++ next.config.js
@@ -12,6 +12,15 @@ const nextConfig = {
       },
     ],
   },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/app/api/trpc/[trpc]/route.tsx
+++ src/app/api/trpc/[trpc]/route.tsx
@@ -1,54 +1,6 @@
 import { appRouter } from "@/trpc";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { NextRequest, NextResponse } from "next/server";
-
-const allowedOrigins = [
-  "https://buy-arena.vercel.app",
-  "https://buy-arena-git-development-vinyldavyls-projects.vercel.app",
-];
-
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  "Access-Control-Allow-Credentials": "true",
-};
-
-export async function OPTIONS(req: NextRequest) {
-  const origin = req.headers.get("origin");
-
-  if (origin && allowedOrigins.includes(origin)) {
-    corsHeaders["Access-Control-Allow-Origin"] = origin;
-  }
-
-  return NextResponse.json({}, { headers: corsHeaders });
-}
-
-async function handler(req: NextRequest) {
-  const origin = req.headers.get("origin");
-
-  if (origin && allowedOrigins.includes(origin)) {
-    corsHeaders["Access-Control-Allow-Origin"] = origin;
-  }
-
-  const response = await fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req: req as unknown as Request,
-    router: appRouter,
-    // @ts-expect-error context already passed from express middleware
-    createContext: () => ({}),
-    onError({ error }) {
-      console.error("Error in tRPC handler:", error);
-    },
-  });
-
-  // Add CORS headers to the response
-  Object.entries(corsHeaders).forEach(([key, value]) => {
-    response.headers.set(key, value);
-  });
-
-  return response;
-}
+import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
   return handler(req);
@@ -56,4 +8,17 @@ export async function GET(req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   return handler(req);
+}
+
+async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    // @ts-expect-error context already passed from express middleware
+    createContext: () => ({}),
+    onError({ error }) {
+      console.error("Error in tRPC handler:", error);
+    },
+  });
 }
