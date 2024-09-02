4644fefcf82f14b65cbf238984d03589c9ad7552
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2024-09-02 21:47:04 +0100

    update: development

--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -4,7 +4,7 @@ import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-export const authRouter: any = router({
+export const authRouter = router({
   createPayloadUser: publicProcedure
     .input(AuthCredentialsValidator)
     .mutation(async ({ input }) => {
