fa52a20544f6735727245690cc9efbb7b6121d3a
Vinyl-Davyl <68241801+Vinyl-Davyl@users.noreply.github.com>
2024-09-03 15:06:57 +0100

    update: development

--- next.config.js
+++ next.config.js
@@ -16,8 +16,29 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "/api/:path*",
+        source: "/api/trpc/:path*",
+        destination: "/api/trpc/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/trpc/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
